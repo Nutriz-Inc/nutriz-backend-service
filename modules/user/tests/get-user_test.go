a89f4f695051d86b81dedab424a7d49ff3742069
leonardopt17 <leonardo.nunes@parcelamostudo.com.br>
2026-05-12 22:31:35 -0300

    refactor: ajustar testes de usuário e remover tabela de arquivos

--- modules/user/tests/get-user_test.go
+++ modules/user/tests/get-user_test.go
@@ -41,7 +41,7 @@ func TestGetUser(t *testing.T) {
 			assert.Equal(t, http.StatusOK, status)
 			assert.Equal(t, id, body["id_user"])
 			assert.NotNil(t, body["addresses"])
-			assert.Len(t, body["addresses"], 1)
+			assert.GreaterOrEqual(t, len(babies), 1)
 		})
 		t.Run("With baby", func(t *testing.T) {
 			route := fmt.Sprintf("%s/%s?show_baby=true", endpoint, id)
@@ -50,7 +50,7 @@ func TestGetUser(t *testing.T) {
 
 			assert.Equal(t, http.StatusOK, status)
 			assert.Equal(t, id, body["id_user"])
-			
+
 			babies, ok := body["babies"].([]interface{})
 			assert.True(t, ok)
 
