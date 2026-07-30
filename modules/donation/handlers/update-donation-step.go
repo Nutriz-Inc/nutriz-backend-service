572b2cfae11b23cae0442afbe4bed5e43f9f79b6
leonardopt17 <leonardo.nunes@parcelamostudo.com.br>
2026-07-29 21:03:01 -0300

    feat: atualizar manipuladores para recuperação de dados, removendo verificações de lista vazia

--- modules/donation/handlers/update-donation-step.go
+++ modules/donation/handlers/update-donation-step.go
@@ -271,7 +271,7 @@ func (h *HandlerUpdateDonationStep) handleAddress(ctx c.Context, tx *sqlx.Tx, da
 }
 
 func (h *HandlerUpdateDonationStep) createAddress(ctx c.Context, data *dto.AddressCreateBase, tx *sqlx.Tx) (*string, *fluxgo.GlobalError) {
-	addressData, err := utils.GetAddressByZipCode(ctx, data.ZipCode, h.config)
+	addressData, err := utils.GetAddressByZipCodeOptionalCoordinates(ctx, data.ZipCode, h.config)
 	if err != nil {
 		return nil, fluxgo.ErrorInternalError(err.Error())
 	}
